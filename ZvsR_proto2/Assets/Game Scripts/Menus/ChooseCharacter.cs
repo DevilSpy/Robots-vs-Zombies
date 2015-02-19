79fef061a501e5e163458250fd001c39fc6e2b0b
unknown <G7601@OHJE-330-WS-05.Labranet.jamk.fi>
2015-02-19 17:03:58 +0200

    Initial commit

---
+++ ZvsR_proto2/Assets/Game Scripts/Menus/ChooseCharacter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChooseCharacter : MonoBehaviour {
+
+	private void OnGUI(){
+		if (GUI.Button (new Rect (Screen.width/2-110, Screen.height/2-35, 100, 70), "Robot")) {
+			Application.LoadLevel ("level1_robot");
+		}
+		if (GUI.Button (new Rect (Screen.width/2+10, Screen.height/2-35, 100, 70), "Zombie")) {
+			Application.LoadLevel ("level1_zombie");
+		}
+	}
+}
