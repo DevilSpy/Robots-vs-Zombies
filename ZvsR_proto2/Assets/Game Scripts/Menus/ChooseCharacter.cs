5e4e2d6cbaed8bc1e39ff021ae0e4ed4e953ba89
Sami Pelkonen <smvpelkonen@gmail.com>
2015-03-02 18:51:24 +0200

    Started on rooftops-scene

--- ZvsR_proto2/Assets/Game Scripts/Menus/ChooseCharacter.cs
+++ ZvsR_proto2/Assets/Game Scripts/Menus/ChooseCharacter.cs
@@ -6,7 +6,7 @@ public class ChooseCharacter : MonoBehaviour {
 	private void OnGUI(){
 		if (GUI.Button (new Rect (Screen.width/2-110, Screen.height/2-35, 100, 70), "Robot")) {
 			GameData.chosenCharacter = "robot";
-			Application.LoadLevel ("level1");
+			Application.LoadLevel ("rooftops");
 		}
 		if (GUI.Button (new Rect (Screen.width/2+10, Screen.height/2-35, 100, 70), "Zombie")) {
 			GameData.chosenCharacter = "zombie";
