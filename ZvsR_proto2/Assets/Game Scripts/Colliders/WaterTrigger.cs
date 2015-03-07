07d1f2a4b797305d22554d3e9fa44fec652bfb38
Sami Pelkonen <smvpelkonen@gmail.com>
2015-03-07 16:38:52 +0200

    Finished mockup of rooftops level, fixed camera angle for Robot

--- ZvsR_proto2/Assets/Game Scripts/Colliders/WaterTrigger.cs
+++ ZvsR_proto2/Assets/Game Scripts/Colliders/WaterTrigger.cs
@@ -5,7 +5,7 @@ public class WaterTrigger : MonoBehaviour {
 
 	void OnTriggerEnter (Collider player){
 		if (GameData.chosenCharacter == "robot") {
-				Application.LoadLevel ("choose_character");
+			Application.LoadLevel ("choose_character");
 		} else {
 			if(!GameData.swimsuitCollected){
 				Application.LoadLevel ("choose_character");
